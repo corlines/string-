5e147b13ddc31575f398844ab1059482d0f5a511
corlines <74294589+corlines@users.noreply.github.com>
2025-01-31 17:28:49 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Net.Mail;
+
+namespace String
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string a = "DDDDDARTI";
+            string b = "TI";
+
+            Console.WriteLine(a); Console.WriteLine(b);
+            
+            Console.WriteLine(a.Concat(b));
+            Console.WriteLine(a.Contains(b));
+            Console.WriteLine(a.IndexOf('A'));
+            Console.WriteLine(a.Insert(3,"OOOOOO"));
+        }
+    }
+    
